c173d1af1094b10f40108655a21499697269979a
niansahc <97317555+niansahc@users.noreply.github.com>
2026-03-15 19:14:21 -0400

    Register generic text file importer

--- src/ingest/importers/__init__.py
+++ src/ingest/importers/__init__.py
@@ -1,7 +1,9 @@
 from .chatgpt import load_chatgpt_export
 from .gdrive import parse_gdrive_files
+from .files import load_text_file
 
 __all__ = [
     "load_chatgpt_export",
     "parse_gdrive_files",
+    "load_text_file",
 ]

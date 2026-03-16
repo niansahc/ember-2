9c46b9a6626548d064bed70fb05e0c752b8844a9
niansahc <97317555+niansahc@users.noreply.github.com>
2026-03-16 13:10:01 -0400

    Register Google Drive download helper

--- src/ingest/importers/__init__.py
+++ src/ingest/importers/__init__.py
@@ -1,13 +1,17 @@
 from .chatgpt import load_chatgpt_export
-from .gdrive import parse_gdrive_files
-from .files import load_text_file
+from .gdrive import list_drive_files, parse_gdrive_files
+from .gdrive_download import download_drive_file
+from .files import load_text_file, load_file
 from .pdf import load_pdf
 from .docx import load_docx
 
 __all__ = [
     "load_chatgpt_export",
+    "list_drive_files",
     "parse_gdrive_files",
+    "download_drive_file",
     "load_text_file",
+    "load_file",
     "load_pdf",
     "load_docx",
 ]
